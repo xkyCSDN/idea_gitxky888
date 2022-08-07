27cb7c1f87d082f59b0d43e80059125d25051ad9
xky888 <282329627@qq.com>
2022-08-07 15:28:47 +0800

    修改2

--- src/main/java/com/atguigu/git/HelloGit.java
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -11,5 +11,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("新建");
         System.out.println("修改1");
+        System.out.println("修改2");
     }
 }
